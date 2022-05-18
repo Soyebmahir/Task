0dcce66a9c35b8c7d3e17357ff34010e954aa21c
Soyeb <tssoyeb@gmail.com>
2022-05-19 03:21:17 +0600

    edited

--- duplicateNumber.js
+++ duplicateNumber.js
@@ -6,14 +6,22 @@ const arr= ["1", "5", "9", "14", "5", "22", "48", "25", "22", "20", "9" ,"13"] *
 const arr = ["1", "5", "9", "14", "5", "22", "48", "25", "22", "20", "9", "13"]
 
 function duplicateNumbers(arr) {
-    let duplicateNumber = []
-    for (let i = 0; i < arr.length; i++) {
+    // let duplicateNumber = []
+    // let count=0;
+    let count ={};
+  /*   for (let i = 0; i < arr.length; i++) {
         if (arr.indexOf(arr[i]) !== i) {
        
-            duplicateNumber.push(arr[i])
+            // duplicateNumber.push(arr[i])
+            count =count+1
         }
-    }
-    return duplicateNumber
+    } */
+    arr.forEach(element => {
+        count[element]=(count[element] || 0)+1;
+        
+    });
+    // return duplicateNumber
+    return count ;
 }
 
 console.log(duplicateNumbers(arr))
